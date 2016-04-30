7326ff17bf2fe213feef4507fd58f565c5cc2da4
david balland <david.balland@pictime.com>
2016-04-30 06:14:48 +0200

    une merde a rebase

--- loulook/src/main/java/org/castors/FirstController.java
+++ loulook/src/main/java/org/castors/FirstController.java
@@ -2,6 +2,7 @@ package org.castors;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.boot.SpringApplication.run;
